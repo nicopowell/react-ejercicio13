723f05a42766dc3eacafa56a6d7fe008907ff47f
Nicolas Powell <nicopowell30@gmail.com>
2023-06-04 23:38:07 -0300

    terminé el maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
 import Formulario from "./components/Formulario";
+import Tarjeta from "./components/Tarjeta";
 import { Container } from "react-bootstrap";
 
 function App() {
   return (
-    <Container>
+    <Container className="d-flex flex-column">
       <h1 className="text-light display-4 text-center mt-4">Web del clima</h1>
       <hr className="text-light"/>
       <Formulario></Formulario>
+      <Tarjeta></Tarjeta>
     </Container>
   )
 }
